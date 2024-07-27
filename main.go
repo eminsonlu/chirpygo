0acd3590ba72fa53ddad9c9c3f97357424ed4f32
Emin Sonlu <eminsnl2@gmail.com>
2024-07-27 21:14:57 +0300

    Server starter

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package chirpygo
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Handler: mux,
+		Addr:    "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+
+	log.Printf("Server started on %s", server.Addr)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
+}
